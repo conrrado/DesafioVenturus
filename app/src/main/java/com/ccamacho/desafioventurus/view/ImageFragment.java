3a8653261ee73ee4575c68b17124043255130da8
conrrado <con.webmaster@gmail.com>
2020-06-04 23:02:44 -0300

    removido imports não usados

--- app/src/main/java/com/ccamacho/desafioventurus/view/ImageFragment.java
+++ app/src/main/java/com/ccamacho/desafioventurus/view/ImageFragment.java
@@ -1,16 +1,14 @@
 package com.ccamacho.desafioventurus.view;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.bumptech.glide.Glide;
 import com.ccamacho.desafioventurus.R;
 import com.ccamacho.desafioventurus.databinding.FragmentImageBinding;
 import com.ccamacho.desafioventurus.model.GalleryData;
